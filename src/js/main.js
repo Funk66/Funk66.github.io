9c27bbd831bd05758279d75cac1920f9e1057f24
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2020-10-31 20:30:55 +0100

    Scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,32 @@
-window.onload = function(e) {
-  console.log("hello onload");
-  window.addEventListener("scroll", (e) => {
-    console.log("Scrolling");
-  });
-};
+var flipping = false;
+var container = null;
+
+window.onload = () => {
+  container = document.getElementById("container");
+}
+
+function scroll (event) {
+  var event = event || window.event;
+  var move = event.deltaY || event.detail;
+  console.log(event);
+  if (move && (
+    (move > 0 && container.className == "") ||
+    (move < 0 && container.className == "reversed")
+  )) {
+    window.flip();
+  }
+}
+
+if ("onmousewheel" in document) {
+  document.onmousewheel = scroll;
+} else {
+  document.addEventListener('DOMMouseScroll', scroll, false);
+}
 
 window.flip = function() {
-  console.log("Reversing");
-  document.getElementById("container").classList.toggle("reversed");
-}
-
-window.showFront = () => {
-  document.getElementById("card").className = "front"
-}
-
-window.showBack = () => {
-  document.getElementById("card").className = "back"
-}
-
-window.scroll = () => {
-  window.scrolling = true;
-}
-
-setInterval(() => {
-  if (window.scrolling) {
-    console.log("scrolling");
-    scrolling = false
-    window.flip()
+  if (!flipping) {
+    flipping = true;
+    container.classList.toggle("reversed");
+    setTimeout(() => flipping = false, 1000);
   }
-}, 1000);
-
-window.scrolling = false;
-
-// flip on scroll down/up
-// global scroll and card scroll (flip up or down)
+}
